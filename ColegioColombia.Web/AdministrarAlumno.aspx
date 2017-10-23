ded6e91d900b359df4947a8a16ff87798d1e11c3
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-10-23 15:32:30 -0500

    crear bd y primer registro.

---
+++ ColegioColombia.Web/AdministrarAlumno.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AdministrarAlumno.aspx.cs" Inherits="ColegioColombia.Web.AdministrarAlumno" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Button ID="btnAdministrarAlumno" runat="server" Text="Guardar" OnClick="btnAdministrarAlumno_Click" />
+</asp:Content>
---
+++ ColegioColombia.Web/AdministrarAlumno.aspx.cs
@@ -0,0 +1,30 @@
+﻿using ColegioColombia.Web.DataAccess;
+using ColegioColombia.Web.Model;
+using System;
+
+namespace ColegioColombia.Web
+{
+    public partial class AdministrarAlumno : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnAdministrarAlumno_Click(object sender, EventArgs e)
+        {
+            var alumno = new Alumno()
+            {
+                Nombre = "Laura",
+                Apellido = "Villa",
+                Cedula = 1038333608,
+                Grado = 11,
+                Grupo = "11C"
+            };
+
+            var context = new ColegioColombiaContext();
+
+            context.Alumno.Add(alumno);
+            context.SaveChanges();
+        }
+    }
+}
