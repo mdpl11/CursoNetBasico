4c2c3a3ead2f3a1bd2a2b33ac541c5e18f5bd2a8
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-10-26 09:39:22 -0500

    Adicionar modelos

---
+++ ColegioColombia.Mvc/Models/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ColegioColombia.Mvc.Models
+{
+    [Table("Alumnos")]
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public long Cedula { get; set; }
+        public int Grado { get; set; }
+        public string Grupo { get; set; }
+    }
+}
---
+++ ColegioColombia.Mvc/Models/Profesor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ColegioColombia.Mvc.Models
+{
+    [Table("Profesores")]
+    public class Profesor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public long Cedula { get; set; }
+        public string Catedra { get; set; }
+    }
+}
