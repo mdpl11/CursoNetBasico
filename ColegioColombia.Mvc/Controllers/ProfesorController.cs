eb893e3c8e61af135b6894c2b36fb4aa26325be4
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-10-31 09:29:33 -0500

    Agregar vista index profesor no tipada

---
+++ ColegioColombia.Mvc/Controllers/ProfesorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColegioColombia.Mvc.Controllers
+{
+    public class ProfesorController : Controller
+    {
+        // GET: Profesor
+        public ActionResult Index()
+        {
+            ViewBag.Bienvenida = "Hola Profesor";
+            return View();
+        }
+    }
+}
