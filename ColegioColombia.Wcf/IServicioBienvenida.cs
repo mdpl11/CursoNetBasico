4c19d82cfdc453acd13bb499223f28dd3357c2aa
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-11-06 22:13:49 -0500

    Crear servicio wcf

---
+++ ColegioColombia.Wcf/IServicioBienvenida.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ColegioColombia.Wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServicioBienvenida" in both code and config file together.
+    [ServiceContract]
+    public interface IServicioBienvenida
+    {
+        [OperationContract]
+        string SaludoPersonalizado(string nombre);
+    }
+}
---
+++ ColegioColombia.Wcf/ServicioBienvenida.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ColegioColombia.Wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServicioBienvenida" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServicioBienvenida.svc or ServicioBienvenida.svc.cs at the Solution Explorer and start debugging.
+    public class ServicioBienvenida : IServicioBienvenida
+    {
+        public string SaludoPersonalizado(string nombre)
+        {
+            int hora = DateTime.Now.Hour;
+
+            nombre = string.IsNullOrEmpty(nombre) ? "Anonimo" : nombre;
+
+            if (hora >= 1 && hora <= 11)
+            {
+                return $"Buenos días {nombre}";
+            }
+            else if (hora >= 12 && hora <= 17)
+            {
+                return $"Buenas tardes {nombre}";
+            }
+
+            return $"Buenas noches {nombre}";
+        }
+    }
+}
