05325694c3930e748a6c8178fa49d4823426d5c9
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-11-06 20:21:32 -0500

    Revert "Configurar proyecto MVC para Web.Api."
    This reverts commit 4853246b2a84621e1c669aedd7631acfb0998e65.

--- ColegioColombia.Mvc/Global.asax.cs
+++ ColegioColombia.Mvc/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,7 +16,6 @@ namespace ColegioColombia.Mvc
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
