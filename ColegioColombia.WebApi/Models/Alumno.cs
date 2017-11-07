75d49aad9aceb4581f54e97876a863f71fff7ca9
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-11-06 20:34:51 -0500

    crear modelos para el webapi

---
+++ ColegioColombia.WebApi/Models/Alumno.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ColegioColombia.Mvc.Models
+{
+    [Table("Alumnos")]
+    public class Alumno
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Apellido { get; set; }
+
+        [Required]
+        public long Cedula { get; set; }
+
+        [Range(6, 11)]
+        [Required]
+        public int Grado { get; set; }
+
+        public string Grupo { get; set; }
+    }
+}
