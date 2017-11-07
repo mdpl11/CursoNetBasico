ddf42d42d8643b65c272b4da1b3925eb798d02f7
Miguel Pelaez <mpelaez@personalsoft.com.co>
2017-11-07 08:08:20 -0500

    Consumir servicio rest del Web.Api

---
+++ ColegioColombia.Cliente/Alumno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColegioColombia.Cliente
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public long Cedula { get; set; }
+
+        public int Grado { get; set; }
+
+        public string Grupo { get; set; }
+    }
+}
---
+++ ColegioColombia.Cliente/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ColegioColombia.Cliente
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            CrearAlumno().Wait();
+            ConsultarAlumnos().Wait();
+
+            Console.ReadLine();
+        }
+
+        private static HttpClient ObtenerCliente()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:58012/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        private static async Task ConsultarAlumnos()
+        {
+            HttpClient client = ObtenerCliente();
+            using (client)
+            {
+                HttpResponseMessage res = await client.GetAsync("api/Alumnos");
+                res.EnsureSuccessStatusCode();
+
+                if (res.IsSuccessStatusCode)
+                {
+                    List<Alumno> alumnos = await res.Content.ReadAsAsync<List<Alumno>>();
+                    Console.WriteLine("");
+                    Console.WriteLine("---------------------Invocando Método GET------------------------");
+                    Console.WriteLine("");
+                    foreach (var alumno in alumnos)
+                    {
+                        Console.WriteLine($"Id: {alumno.Id} - Nombre: {alumno.Nombre} - Apellido: {alumno.Apellido}");
+                    }
+                    Console.WriteLine("-----------------------------------------------------------");
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        private static async Task CrearAlumno()
+        {
+            HttpClient client = ObtenerCliente();
+            using (client)
+            {
+                Alumno alumno = new Alumno
+                {
+                    Nombre = "Juan",
+                    Apellido = "Rodriguez",
+                    Cedula = 15478475689,
+                    Grado = 11,
+                    Grupo = "11C"
+                };
+
+                Console.WriteLine("");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("------------------Invocando Método Post--------------------");
+
+                HttpResponseMessage res = await client.PostAsJsonAsync("api/Alumnos", alumno);
+                res.EnsureSuccessStatusCode();
+
+                if (res.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("------------------Alumno Creado Exitosamente--------------------");
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
